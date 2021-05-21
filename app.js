874563442cbc570b696520d69c72cf86240f7f74
RenatoMignella <renatoferigato@gmail.com>
2021-05-21 14:25:15 +0100

    && instead of || added on the if's

--- app.js
+++ app.js
@@ -71,10 +71,11 @@ const n = weekday[day.getDay()];
 //week  B is equal Thu and Fr
 
 console.log(week);
-if ((week === 'b' && n === 'Thursday') || n === 'Friday') {
+console.log(n);
+if (week === 'b' && n === 'Thursday' && n === 'Friday') {
      inOffice.textContent = 'In office today (3PP)';
      inOffice.style.color = 'green';
-} else if ((week === 'a' && n === 'Monday') || n === 'Tuesday' || n === 'Wednesday') {
+} else if (week === 'a' && n === 'Monday' && n === 'Tuesday' && n === 'Wednesday') {
      inOffice.textContent = 'In office today (3PP)';
      inOffice.style.color = 'green';
 } else {
